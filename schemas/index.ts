262fffbf7fdf2f9e353af3aeb1088898d373e108
mozahid <ce.muzahid@gmail.com>
2023-10-31 09:57:54 +0600

    initial commit

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import about from './about'
+import brand from './brand'
+import contact from './contact'
+import experience from './experience'
+import skills from './skills'
+import testimonials from './testimonials'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [
+  about,
+  brand,
+  contact,
+  experience,
+  skills,
+  testimonials,
+  workExperience,
+  works,
+]
